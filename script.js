39d072b0291b7681fb449972bdea00ea296f41c4
Roman Nazarenko <justaspeckinspace@gmail.com>
2024-02-07 23:54:24 +0300

    Add blank script.js file

---
+++ script.js
@@ -0,0 +1,11 @@
+// Select elements
+const form = document.getElementById('todoform');
+
+// Form submit
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  console.log('Hello');
+});
+
+/* 12:36 */
