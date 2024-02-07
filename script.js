7d82946d78f496fedf0af1ba81520a652d9a6ffb
Roman Nazarenko <justaspeckinspace@gmail.com>
2024-02-07 23:54:24 +0300

    Add blank script.js file

---
+++ script.js
@@ -0,0 +1,13 @@
+// https://www.youtube.com/watch?v=i1pxPSl9ZHc&list=WL&index=26
+
+// Select elements
+const form = document.getElementById('todoform');
+
+// Form submit
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  console.log('Hello');
+});
+
+/* 12:36 */
